d86237adf4ad391fad9b230f048fe526b6ae82fd
justinlee96jl <justinlee96.jl@gmail.com>
2018-12-12 18:12:00 -0800

    Create CONSTANTS.py

---
+++ CONSTANTS.py
@@ -0,0 +1,11 @@
+#CONSTANTS MODULE WITH ALL CONSTANTS USED IN CRYPTO SCRIPTS
+
+KEY = 16
+BLOCK = 128
+BYTELENGTH = 32
+MESSAGE = "This message is not encrypted anymore!"
+directory= 'C:/Users/*****/*****/cecs378Pyfiles'
+file_path = 'C:/Users/*****/*****/cecs378Pyfiles/testfile.txt'
+jpeg_path = 'C:/Users/*****/*****/cecs378Pyfiles/test.jpeg'
+destination = "/Users/*****/*****/cecs378Pyfiles"
+testDirectory = 'C:/Users/*****/*****/testDirectory'
